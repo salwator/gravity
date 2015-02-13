cea2c209e1b995dfaac77fdef58a60cf38140a37
nondefinitive <salwator@gmail.com>
2015-02-13 23:45:55 +0100

    minor main refactor

--- gravity.cpp
+++ gravity.cpp
@@ -4,30 +4,41 @@
 #include "space.h"
 #include "newton_simulator.h"
 
+namespace
+{
+
 Planet on_others_orbit(Planet& central, double distance, double mass)
 {
-    auto planet = Planet(mass, Vector(central.x, central.y + distance), Vector(sqrt((G*central.mass)/distance)+central.vx,central.vy));
-    return planet;
+    return Planet(mass, Vector(central.x, central.y + distance),
+                  Vector(sqrt((G*central.mass) / distance) + central.vx,
+                         central.vy));
+}
+
+Planets sample_planets()
+{
+    auto planets = Planets();
+    planets.push_back(Planet(20000000,Vector(0,0),Vector(0,0)));
+    planets.push_back(on_others_orbit(planets[0], 1000, 10000));
+    planets.push_back(on_others_orbit(planets[1], 20, 10));
+    return planets;
+}
+
 }
 
 int main()
 {
-    auto planets = Planets();
-    planets.push_back(Planet(10000000,Vector(0,0),Vector(0,0)));
-    planets.push_back(on_others_orbit(planets[0], 1000, 10000));
-    planets.push_back(on_others_orbit(planets[1], 20, 10));
+    auto simulation = NewtonSimulator(sample_planets(), 10);
 
-    auto simulation = NewtonSimulator(planets, 10);
-
-    auto i = 0;
-    while(i++ < 100000)
+    for(auto i = 0; i < 100000; i++)
     {
         simulation.simulate(10);
+
         for(const auto & planet : simulation.result())
         {
             std::cout << planet.x << "," << planet.y << ",";
         }
-    std::cout << std::endl;
+
+        std::cout << std::endl;
     }
 
 }
