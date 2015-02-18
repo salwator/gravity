2c45a40c46be84a3c8045e0be1b8551b1b7f9e3c
nondefinitive <salwator@gmail.com>
2015-02-18 22:46:15 +0100

    threading procedure refactored

--- simulation/simulate_threaded.cpp
+++ simulation/simulate_threaded.cpp
@@ -3,20 +3,25 @@
 #include <future>
 #include <iostream>
 
-Planets simulate_threaded(const Planets & planets, const units::base_time dt, int threads_num)
+
+namespace
 {
-    auto futures = std::vector<std::future<Planets>>();
-    auto partial_planets = std::vector<std::vector<std::reference_wrapper<const Planet>>>(threads_num);
+
+typedef std::vector<std::reference_wrapper<const Planet>> PlanetRefList;
+
+std::vector<PlanetRefList> distribute_all_planets(const Planets & planets, int threads_num)
+{
+    auto distributed = std::vector<PlanetRefList>(threads_num);
 
     auto count = 0;
     for(const auto & planet : planets)
-        partial_planets[count++ % threads_num].push_back(std::cref(planet));
+        distributed[count++ % threads_num].push_back(std::cref(planet));
 
-    for(auto i = 0; i < threads_num; i++)
-        futures.push_back(std::async(std::launch::async, [&](int i){
-                                                                        return simulate_range(planets, partial_planets[i], dt);
-                                                                   }, i));
+    return distributed;
+}
 
+Planets join_result_after_calculated(std::vector<std::future<Planets>> & futures)
+{
     auto planets_new = Planets();
 
     for(auto & future_result : futures)
@@ -26,4 +31,19 @@ Planets simulate_threaded(const Planets & planets, const units::base_time dt, in
     }
 
     return planets_new;
+}
+
+} // namespace
+
+Planets simulate_threaded(const Planets & planets, const units::base_time dt, int threads_num)
+{
+    auto partial_planets = distribute_all_planets(planets, threads_num);
+    auto futures = std::vector<std::future<Planets>>();
+
+    for(auto i = 0; i < threads_num; i++)
+        futures.push_back(std::async(std::launch::async, [&](int i){
+                                                                        return simulate_range(planets, partial_planets[i], dt);
+                                                                   }, i));
+
+    return join_result_after_calculated(futures);
 }
