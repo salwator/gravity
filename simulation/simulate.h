5d25670961efb0695fe08635db0557d7bd981cd4
Jakub Draganek <salwator@gmail.com>
2015-02-20 23:42:36 +0100

    unused prototype remove

--- simulation/simulate.h
+++ simulation/simulate.h
@@ -7,5 +7,4 @@ const auto G = 6.67384E-11;
 
 Planets simulate(const Planets&, units::base_time = DELTA_TIME);
 Planets simulate_threaded(const Planets &, const units::base_time, int);
-Planets simulate_threaded_std(const Planets &, const units::base_time, int);
 Planets simulate_range(const Planets &, const std::vector<std::reference_wrapper<const Planet>> &, units::base_time);
